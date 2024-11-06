d55567a07262e65d88549d4ff2a6be62286c0174
Garrett <hawkpdx@icloud.com>
2024-11-05 19:58:51 -0800

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('%c HI', 'color: firebrick')
+fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then((response) => response.json())
+  .then((data) => {
+    const dogImageContainer = document.getElementById("dog-image-container");
+    data.message.forEach((dog) => {
+      const dogImage = document.getElementById("dog-image-container");
+      dogImage.src = dog;
+      dogImageContainer.appendChild(dogImage);
+    });
+  });
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+fetch("https://dog.ceo/api/breeds/list/all")
+  .then((response) => response.json())
+  .then((data) => {
+    const dogBreeds = document.getElementById("select-breed");
+    const dogBreedContainer = document.getElementById("dog-breeds");
+
+    for (let breed in dogBreeds) {
+      const listItem = document.createElement("li");
+      listItem.textContent = breed;
+      dogBreedContainer.appendChild(listItem);
+    }
+  });
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
